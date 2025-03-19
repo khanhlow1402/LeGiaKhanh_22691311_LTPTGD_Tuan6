c9114b841634f6a08b01cffd2deb90778e973169
khanhlow1402 <legiakhanhtrongsang@gmail.com>
2025-03-19 11:21:56 +0700

    up

--- bai1/src/App.jsx
+++ bai1/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function TangGia(){
+    
+      const initialState = {count: 0};
+    
+      const reducer = (state, action) => {
+        switch (action.type) {
+            case '+':
+                return {...state, count: state.count + action.value};
+            case '-':
+                return {...state, count: state.count - action.value};
+            default:
+                return state;
+        }
+      }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      const [state, dispatch] = useReducer(reducer, initialState);
+      const handleChange = (type) =>{
+        var value = 1;
+        console.log(type);
+        dispatch({type, value});
+      }
+    
+    return(
+        <div className="wrapper" style={{padding: 50}}>
+            <h1>Tăng giảm</h1>
+            <span >{state.count}</span>
+            <br />
+            <button 
+                onClick={() => handleChange('+')}
+            >
+                Increase
+            </button>
+            <button
+                onClick={() => handleChange('-')}
+            >
+                Decrease
+            </button>
+        </div>
+    )
 }
-
-export default App
